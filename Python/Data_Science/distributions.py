d6ffca038d5d16811978570102fa56df5424d071
Ismail Atasoy <ismailatasoy85@gmail.com>
2025-12-01 12:30:16 +0300

    Add examples for Chi Square, Rayleigh, Pareto, and Zipf distributions in data visualization

--- Python/Data_Science/distributions.py
+++ Python/Data_Science/distributions.py
@@ -77,3 +77,57 @@ plt.show()
 #assume that you are in a bank. while time passing probabilty of new customers decreasing.
 '''
 
+#Chi Square
+'''
+data = random.chisquare(df=5,size=100)
+#taking random 5 product and look their scale. sum of squares of scales will be among df value.
+print(data)
+sb.histplot(data,bins=30,kde=True)
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Chi Square Distribution")
+plt.show()
+#if value low its good for the product but if it highs it is bad. 
+'''
+
+#Rayleigh 
+'''
+data = random.rayleigh(scale=2.0,size=100)
+#scale is like wave height. 
+print(data)
+
+sb.histplot(data,bins=30,kde=True)
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Rayleigh Distribution")
+plt.show()
+'''
+
+#Pareto
+'''
+xm = 1.0 #start value
+data = (random.pareto(2.0,1000)+1)*xm
+print(data)
+
+sb.histplot(data,bins=40,kde=True)
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Pareto Distribution (p=2.0)")
+plt.show()
+#imagine that it is an income if p increases it will be more balanced 
+'''
+
+#Zipf
+'''
+data = random.zipf(2.0,1000)
+data = data[10>data]
+print(data)
+
+sb.histplot(data,bins=range(1,11),discrete=True)
+plt.xlabel("Rank")
+plt.ylabel("Frequency")
+plt.title("Zipf Distribution (s=2.0)")
+plt.show()
+#Frequency will decrease continiously. example(60x 30x 20x 15x 12x 10x)  
+'''
+
