416ca59d168f2ebb4647f40a39245d26f3d0ef9a
Ismail Atasoy <ismailatasoy85@gmail.com>
2025-12-24 11:13:30 +0300

    .

---
+++ Python/Data_Science/scipy/constants.py
@@ -0,0 +1,8 @@
+from scipy import constants
+ 
+print(dir(constants)) #reach all constants here
+
+kg = 5 * constants.pound
+print(f"5 pound = {kg}kg")
+
+#All constants return as SI unit system as example above
